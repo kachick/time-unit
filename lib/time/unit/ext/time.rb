ba89db037253b109afaef4a0fe46d61bceea2879
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-03-23 00:41:53 +0900

    modify file tree

---
+++ lib/time/unit/ext/time.rb
@@ -0,0 +1,19 @@
+require 'forwardable'
+
+module Time::Unit::TimeExtention
+  module EigenMethod
+    extend Forwardable
+
+    class << self
+      private(*Forwardable.instance_methods(false))
+    end
+
+    ##
+    #
+    def_delegator ::Time::Unit, :new, :Unit
+  end
+end
+
+class Time
+  extend Time::Unit::TimeExtention::EigenMethod
+end
